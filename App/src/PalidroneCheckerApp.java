217ebb270febd437b51bc9a90037da79ee04421a
Ovieez-stack <ov2571@srmist.edu.in>
2026-02-23 16:11:49 +0530

    hello palindrome

--- App/src/PalidroneCheckerApp.java
+++ App/src/PalidroneCheckerApp.java
@@ -1,27 +1,12 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class PalidroneCheckerApp {
 
-    public static boolean isPalindrome(String input) {
-        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
-        String reversed = new StringBuilder(cleaned).reverse().toString();
-        return cleaned.equals(reversed);
-    }
-
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.print("Enter a word or sentence: ");
-
-        String userInput = scanner.nextLine();
-
-        if (isPalindrome(userInput)) {
-            System.out.println("It is a Palindrome ✅");
-        } else {
-            System.out.println("It is NOT a Palindrome ❌");
-        }
-
-        scanner.close();
+        System.out.println("Welcome To The Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
     }
 }
